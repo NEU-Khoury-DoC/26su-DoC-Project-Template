9614e0398c4a8b5309ac6a01c17f7a2870f67745
Ari Spokony <spokonya@outlook.com>
2026-06-03 20:08:28 +0200

    inital landing page

--- app/src/Home.py
+++ app/src/Home.py
@@ -1,72 +1,59 @@
 ##################################################
-# This is the main/entry-point file for the
-# sample application for your project
+# Main entry-point for the Zeus Streamlit app
 ##################################################
 
-# Set up basic logging infrastructure
 import logging
-logging.basicConfig(format='%(filename)s:%(lineno)s:%(levelname)s -- %(message)s', level=logging.INFO)
+
+logging.basicConfig(
+    format="%(filename)s:%(lineno)s:%(levelname)s -- %(message)s",
+    level=logging.INFO,
+)
 logger = logging.getLogger(__name__)
 
-# import the main streamlit library as well
-# as SideBarLinks function from src/modules folder
 import streamlit as st
 from modules.nav import SideBarLinks
 
-# streamlit supports regular and wide layout (how the controls
-# are organized/displayed on the screen).
-st.set_page_config(layout='wide')
+st.set_page_config(layout="wide")
 
-# If a user is at this page, we assume they are not
-# authenticated.  So we change the 'authenticated' value
-# in the streamlit session_state to false.
-st.session_state['authenticated'] = False
+# User is on the login page — not authenticated yet
+st.session_state["authenticated"] = False
 
-# Use the SideBarLinks function from src/modules/nav.py to control
-# the links displayed on the left-side panel.
-# IMPORTANT: ensure src/.streamlit/config.toml sets
-# showSidebarNavigation = false in the [client] section
 SideBarLinks(show_home=True)
 
-# ***************************************************
-#    The major content of this page
-# ***************************************************
-
 logger.info("Loading the Home page of the app")
-st.title('Summer 2026 Belgium DoC Project Template')
-st.write('#### Hi! As which user would you like to log in?')
 
-# For each of the user personas for which we are implementing
-# functionality, we put a button on the screen that the user
-# can click to MIMIC logging in as that mock user.
+st.title("Zeus Energy Security Index")
+st.write("Choose a persona, then log in.")
 
-if st.button("Act as John, a Political Strategy Advisor",
-             type='primary',
-             use_container_width=True):
-    # when user clicks the button, they are now considered authenticated
-    st.session_state['authenticated'] = True
-    # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
-    # we add the first name of the user (so it can be displayed on
-    # subsequent pages).
-    st.session_state['first_name'] = 'John'
-    # finally, we ask streamlit to switch to another page, in this case, the
-    # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/00_Pol_Strat_Home.py')
+col_household, col_journalist, col_analyst, col_login = st.columns([2, 2, 2, 1])
 
-if st.button('Act as Mohammad, a USAID Worker',
-             type='primary',
-             use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
+with col_household:
+    st.markdown("**Household Owner**")
+    st.selectbox(
+        "Household Owner options",
+        options=["Select an option", "Option 1", "Option 2"],
+        key="household_owner_dropdown",
+        label_visibility="collapsed",
+    )
 
-if st.button('Act as System Administrator',
-             type='primary',
-             use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'administrator'
-    st.session_state['first_name'] = 'SysAdmin'
-    st.switch_page('pages/20_Admin_Home.py')
+with col_journalist:
+    st.markdown("**Journalist**")
+    st.selectbox(
+        "Journalist options",
+        options=["Select an option", "Option 1", "Option 2"],
+        key="journalist_dropdown",
+        label_visibility="collapsed",
+    )
+
+with col_analyst:
+    st.markdown("**Analyst**")
+    st.selectbox(
+        "Analyst options",
+        options=["Select an option", "Option 1", "Option 2"],
+        key="analyst_dropdown",
+        label_visibility="collapsed",
+    )
+
+with col_login:
+    st.markdown("&nbsp;", unsafe_allow_html=True)
+    st.button("Log in", type="primary", use_container_width=True)
