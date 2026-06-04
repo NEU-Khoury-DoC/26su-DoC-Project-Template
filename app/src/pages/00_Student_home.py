ede0ec8fa463a6be691fbaac76fbbc7183f7cb99
laasyagattu <gattu.l@northeastern.edu>
2026-06-05 00:52:37 +0200

    streamlit page for ml model student and ml model routes

--- app/src/pages/00_Student_home.py
+++ app/src/pages/00_Student_home.py
@@ -12,12 +12,17 @@ SideBarLinks()
 st.title(f"Welcome Student, {st.session_state['name']}.")
 st.write('### What would you like to do today?')
 
-if st.button('View listings',
+if st.button('View Listings',
              type='primary',
              use_container_width=True):
     st.switch_page('pages/01_view_listings_student.py')
 
-if st.button('View budget manager',
+if st.button('View Budget Manager',
              type='primary',
              use_container_width=True):
     st.switch_page('pages/02_budget_manager.py')
+
+if st.button('Housing Satisfaction Predictor',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/04_student_pred.py')
