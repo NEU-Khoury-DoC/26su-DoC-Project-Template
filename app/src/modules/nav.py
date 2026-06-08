e639eb730ac7c51f6183f865a6ec8b17ab75f5b9
Maira Padani <padani.m@northeastern.edu>
2026-06-08 10:56:05 +0200

    logo changes

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -74,7 +74,7 @@ def SideBarLinks(show_home=False):
     """
 
     # Logo appears at the top of the sidebar on every page
-    st.sidebar.image("assets/logo.png", width=300)
+    st.sidebar.image("assets/LogoNew.png", width=300)
 
     # If no one is logged in, send them to the Home (login) page
     if "authenticated" not in st.session_state:
