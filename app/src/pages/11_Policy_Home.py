ab9b61673b311083c33cc795b2cc4b0c966ff12a
Lauryn Gong <gong.la@northeastern.edu>
2026-06-10 19:05:11 +0200

    fixing links

--- app/src/pages/11_Policy_Home.py
+++ app/src/pages/11_Policy_Home.py
@@ -15,17 +15,17 @@ st.write('### What would you like to do today?')
 if st.button('View Crop Map',
              type='primary',
              use_container_width=True):
-    st.switch_page('pages/11_Policy_Map.py')
+    st.switch_page('pages/12_Policy_Map.py')
 
 if st.button('View Compare Farms',
              type='primary',
              use_container_width=True):
-    st.switch_page('pages/12_Policy_Compare.py')
+    st.switch_page('pages/13_Policy_Compare.py')
 
 if st.button('View Report Maker',
              type='primary',
              use_container_width=True):
-    st.switch_page('pages/13_Policy_Report.py')
+    st.switch_page('pages/14_Policy_Report.py')
 
 if st.button('View Crop Price Predictions',
              type='primary',
