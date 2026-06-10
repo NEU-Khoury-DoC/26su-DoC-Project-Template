88bc2b425b296fc94eac1d59851845750a81694c
Lauryn Gong <gong.la@northeastern.edu>
2026-06-10 18:57:22 +0200

    policymaker landing page

--- app/src/pages/11_Policy_Home.py
+++ app/src/pages/11_Policy_Home.py
@@ -11,3 +11,28 @@ SideBarLinks()
 
 st.title(f"Welcome Policy Maker, {st.session_state['first_name']}.")
 st.write('### What would you like to do today?')
+
+if st.button('View Crop Map',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/11_Policy_Map.py')
+
+if st.button('View Compare Farms',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/12_Policy_Compare.py')
+
+if st.button('View Report Maker',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/13_Policy_Report.py')
+
+if st.button('View Crop Price Predictions',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/15_Policy_Predictions.py')
+
+if st.button('View Discussion Board',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/17_Policy_Blog.py')
